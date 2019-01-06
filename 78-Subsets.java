027a6469a9203951c4fc2f80b65a385a4509a837
hwhollywu <hwhollywu@gmail.com>
2019-01-06 15:33:46 -0500

    90

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -22,5 +22,49 @@ class Solution {
     
 }
 
+// 2ms answer
 
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<Integer>());
+        //Arrays.sort(nums);
+        for(int n : nums) {
+            List<List<Integer>> tmp = new ArrayList<>();
+            for(List<Integer> sub : res) {
+                List<Integer> a = new ArrayList<>(sub);
+                a.add(n);
+                tmp.add(a);
+            }
+            res.addAll(tmp);
+        }
+        return res;
+    }
+}
+
+
+90-Subsets II
+// https://leetcode.com/problems/subsets-ii/description/
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        subsetsWithDupRec(res, new ArrayList<>(), nums, visited, 0);
+        return res;
+    }
+    
+    private void subsetsWithDupRec(List<List<Integer>> lists, List<Integer> temp, int[] nums, boolean[] visited, int start){
+        lists.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++){
+            if (i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            temp.add(nums[i]);
+            visited[i] = true;
+            subsetsWithDupRec(lists, temp, nums, visited, i+1);
+            temp.remove(temp.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
 
