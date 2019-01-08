1a9efd972406866030b3c8427891e3e893a3b18b
hwhollywu <hwhollywu@gmail.com>
2019-01-08 14:22:53 -0500

    118.119-PascalsTriangle.java

--- 118.119-PascalsTriangle.java
+++ 118.119-PascalsTriangle.java
@@ -2,7 +2,46 @@
 
 // https://leetcode.com/problems/pascals-triangle/
 
+/*
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+// Time Complexity O(numRows^2)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList();
+        if (numRows <= 0) return res;
+        res.add(new ArrayList<>());
+        res.get(0).add(1);
+        for (int i = 1; i < numRows; i++){
+            List<Integer> list = new ArrayList();
+            List<Integer> last = res.get(i-1);
+            // construct list
+            list.add(1); // first ele
+            for (int j = 1; j < i; j++){
+                list.add(last.get(j-1) + last.get(j));
+            }
+            list.add(1); // last ele
+            res.add(list);
+        }
+        return res;
+    }
+}
+
 // 119. Pascal's Triangle 
+// https://leetcode.com/problems/pascals-triangle-ii/
+/*
+Input: 3
+Output: [1,3,3,1]
+*/
 
 class Solution {
     public List<Integer> getRow(int rowIndex) {
@@ -21,33 +60,16 @@ class Solution {
 }
 
 
-
-// Time Complexity O(numRows^2)
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        // base case
-        if (numRows == 0) return res;
-        // base case 2
-        res.add(new ArrayList<>());
-        res.get(0).add(1);
-        if (numRows == 1) return res;
-        // recursive case
-        for (int i = 1; i < numRows; i++){
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prev = res.get(i-1);
-            row.add(1); // first element of all rows
-            // calculate each element in the new row
-            // = sum of above and to the left
-            // + above and to the right
-            for (int j = 1; j < i ; j ++){
-                row.add(prev.get(j-1) + prev.get(j));
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList();
+        for(int i = 0; i <= rowIndex; i++){
+            List<Integer> temp =  new ArrayList<>(res); // clone arraylist
+            res.add(1);
+            for (int j = 1; j < i; j++){
+                res.set(j, temp.get(j) + temp.get(j-1));
             }
-            row.add(1); // last element
-            res.add(row);
         }
         return res;
-            
-        
     }
 }
