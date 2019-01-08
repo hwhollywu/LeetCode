a25677b9c8fbe6da20713a9f3452711e85ed2d41
hwhollywu <hwhollywu@gmail.com>
2019-01-07 23:49:38 -0500

    rob update

--- 198.213.337-HouseRobber.java
+++ 198.213.337-HouseRobber.java
@@ -3,30 +3,29 @@
 // https://leetcode.com/problems/house-robber/
 
 
-// Wrong!
-class Solution {
-    public int rob(int[] nums) {
-        if (nums.length < 1) return 0; // base case
-        int even = nums[0];
-        if (nums.length == 1) return nums[0]; // base case
-        int odd = nums[1];
-        if (nums.length == 2) return Math.max(even,odd);  // base cases
-        for (int i = 2; i < nums.length; i++){
-            if (i%2 == 0) even += nums[i];
-            else odd += nums[i];
-        }
-        return Math.max(even,odd);
-    }
-}
+/*
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+*/
+
+
 /// USE DP
 class Solution {
     public int rob(int[] nums) {
         // base cases
         if (nums.length < 1) return 0;
-        if (nums.length == 1) return nums[0]; 
-        if (nums.length == 2) return Math.max(nums[0],nums[1]); 
         int[] dp = new int[nums.length];
         dp[0] = nums[0]; 
+        if (nums.length == 1) return dp[0]; 
         dp[1] = Math.max(nums[0],nums[1]); 
         for (int i = 2; i < nums.length; i++){
             dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
@@ -35,8 +34,38 @@ class Solution {
     }
 }
 
+// Space O（1）
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length < 1) return 0;
+        int pre2 = 0;
+        int pre1 = 0;
+        for (int i = 0; i < nums.length; i++){
+            int cur = Math.max(pre2 + nums[i], pre1);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1; // n-1
+    }
+}
+
 // 213 
 // https://leetcode.com/problems/house-robber-ii/
+
+/*
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+*/
+
+
 // Circle ! first and last are neighbors
 
 class Solution {
@@ -64,10 +93,60 @@ class Solution {
     }
 }
 
+// use 198 as helper function
+
+class Solution {
+    public int rob(int[] nums) {
+        // base cases
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        // compare rob first, and rob end
+        return Math.max(robHelper(nums, 0, n-1), robHelper(nums, 1, n));
+    }
+    
+    public int robHelper(int[] nums, int start, int end) {
+        int pre2 = 0;
+        int pre1 = 0;
+        for (int i = start; i < end; i++){
+            int cur = Math.max(pre2 + nums[i], pre1);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
+    }
+}
+
 
 // 337. House Robber III
 // sum of not directly-linked Binary Tree!
 // https://leetcode.com/problems/house-robber-iii/
+
+/*
+Input: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+Output: 7 
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+
+Input: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+*/
+
 
 /**
  * Definition for a binary tree node.
@@ -102,6 +181,7 @@ class Solution {
 
 // post order traversal = DFS = Recursion, DP
 // https://leetcode.com/problems/house-robber-iii/discuss/184212/Java-Post-Order-Traversal
+
 
 class Solution {
         
