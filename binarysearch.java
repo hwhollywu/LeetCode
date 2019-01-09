c39020a76c96e9704c939049a8520d87a94ae992
hwhollywu <hwhollywu@gmail.com>
2019-01-09 01:04:28 -0500

    34

--- binarysearch.java
+++ binarysearch.java
@@ -16,6 +16,20 @@ public int binarySearch(int[] nums, int key) {
     return -1;
 }
 
+// 在一个有重复元素的数组中查找 key 的最左位置
+public int binarySearch(int[] nums, int key) {
+    int l = 0, h = nums.length - 1;
+    while (l < h) {
+        int m = l + (h - l) / 2;
+        if (nums[m] >= key) {
+            h = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+}
+
 
 69. Sqrt(x)
 // https://leetcode.com/problems/sqrtx/
@@ -91,9 +105,105 @@ class Solution {
 540. Single Element in a Sorted Array
 // https://leetcode.com/problems/single-element-in-a-sorted-array/description/
 
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r){
+            int m = l + (r - l) / 2;
+            if (m % 2 == 1) m -= 1; // make m always even
+            if (nums[m] == nums[m+1]){
+                // target appears after
+                l = m + 2;
+            }else{
+                // target appears in or after 
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+}
+
+
+278. First Bad Version
+// https://leetcode.com/problems/first-bad-version/
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        while (l < r){
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)){
+                // target appear before or at m
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
+
+
+153. Find Minimum in Rotated Sorted Array
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r){
+            int m = l + (r - l) / 2;
+            if (nums[m] <= nums[r]){
+                r = m;
+            }else {
+                l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+}
 
 
 
+34. Find First and Last Position of Element in Sorted Array
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
+/*
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
 
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
 
+*/
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearch(nums, target);
+        int last = binarySearch(nums, target + 1) - 1;
+        // not found case:
+        if (first == nums.length || nums[first] != target){
+            return new int[]{-1,-1};
+        }
+        return new int[]{first, last};
+    }
+    
+    private int binarySearch(int[] nums, int target){
+        int l = 0;
+        int r = nums.length;
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if (nums[m] >= target){
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }
+        return l; // l>=r
+    }
+}
