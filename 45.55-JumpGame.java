efdac4517813a8d4f8c41dace7f9fc3674065343
hwhollywu <hwhollywu@gmail.com>
2019-01-13 15:58:02 -0500

    45.55-JumpGame.java

--- 45.55-JumpGame.java
+++ 45.55-JumpGame.java
@@ -16,6 +16,8 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
 
 // Complexity: Time O(n), Space O(1) 
 
+// Approach Greedy: from the start, select the local optimum to reach the global optimum
+
 public boolean canJump(int[] nums) {
     int max = 0;
     for(int i=0;i<nums.length;i++){
@@ -26,4 +28,77 @@ public boolean canJump(int[] nums) {
 }
 
 
-// Approach DP:
+// Approach DP: from end to head
+
+public boolean canJump(int[] nums) {
+	int n = nums.length;
+	boolean[] jump = boolean[n];
+	jump[n - 1] = true;
+	for(int i = n-2; i >= 0; i--){
+		// for each nums[i]
+		for(int j = 0; j <= nums[i] && i+j<n; j++){
+			if (jump[i+j] == true){
+				jump[i] = true;
+				break;
+			}
+		}
+	}
+	return jump[0];
+}
+
+
+
+// https://leetcode.com/problems/jump-game-ii/
+
+/*
+
+Input: [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2.
+    Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+*/
+
+// Approach Greedy O(n)
+
+public int jump(int[] nums) {
+	int jumps = 0;
+	int curEnd = 0; 
+	int max = 0; 
+	for(int i = 0; i < nums.length - 1; i++){
+		max = Math.max(max, i + nums[i]);
+		// reach end, start another jump
+		if (i == curEnd){
+			jumps++;
+			curEnd = max;
+		}
+
+	}
+	return jumps;
+}
+
+// Approach BFS
+
+public int jump(int[] nums) {
+	int n = nums.length;
+	if (n < 2) return 0; // base 
+	Map<Integer, Integer> map = new HashMap(); // index - jumps
+	Queue<Integer> q = new LinkedList();
+	q.add(0);
+	map.put(0,0);
+	while(q.size() != 0){
+		int index = q.remove();
+		int jumps = map.get(index);
+		for(int i = nums[index]; i > 0; i--){
+			int next = index + i;
+			if (next >= n - 1) return jumps + 1;
+			if (!map.containsKey(next)){
+				map.put(next, jumps + 1);
+				q.add(next);
+			}
+		}
+	}
+	return -1;
+}
+
+
