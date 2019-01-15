1e2175ff6d41ca613d75001d22b795b8bc833068
hwhollywu <hwhollywu@gmail.com>
2019-01-14 22:26:29 -0500

    684.685-RedundantConnection.java

---
+++ 684.685-RedundantConnection.java
@@ -0,0 +1,208 @@
+684. Redundant Connection
+
+// https://leetcode.com/problems/redundant-connection/description/
+
+/*
+
+Return an edge that can be removed so that the resulting graph is 
+a tree of N nodes. If there are multiple answers, return the answer 
+that occurs last in the given 2D-array. 
+
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+Explanation: The given undirected graph will be like this:
+  1
+ / \
+2 - 3
+
+Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+Output: [1,4]
+Explanation: The given undirected graph will be like this:
+5 - 1 - 2
+    |   |
+    4 - 3
+
+*/
+
+
+// class union find
+
+class UnionFind{
+    private int[] id;
+    UnionFind(int n){
+        id = new int[n+1];
+        for(int i = 0; i < id.length; i++) id[i] = i;
+    }
+    
+    int find(int a){
+        return id[a];
+    }
+    
+    boolean connect(int u, int v){
+        return find(u) == find(v);
+    }
+    
+    void union(int u, int v){
+        int uid = find(u);
+        int vid = find(v);
+        if(uid == vid) return;
+        for(int i = 0; i < id.length; i++){
+            if(id[i] == uid) id[i] = vid;
+        }
+    }
+}
+
+
+// Time & Space: O(N) O(N)  # nodes / edges
+
+// Approach 1 : Union Find
+public int[] findRedundantConnection(int[][] edges) {
+    int N = edges.length;
+    UF uf = new UF(N);
+    for (int[] e : edges) {
+        int u = e[0], v = e[1];
+        if (uf.connect(u, v)) {
+            return e;
+        }
+        uf.union(u, v);
+    }
+    return new int[]{-1, -1};
+}
+
+
+// Approach 2: DFS 
+
+class Solution {
+    Set<Integer> seen = new HashSet();
+    int MAX_EDGE_VAL = 1000;
+
+    public int[] findRedundantConnection(int[][] edges) {
+        ArrayList<Integer>[] graph = new ArrayList[MAX_EDGE_VAL + 1];
+        for (int i = 0; i <= MAX_EDGE_VAL; i++) {
+            graph[i] = new ArrayList();
+        }
+
+        for (int[] edge: edges) {
+            seen.clear();
+            if (!graph[edge[0]].isEmpty() && !graph[edge[1]].isEmpty() &&
+                    dfs(graph, edge[0], edge[1])) {
+                return edge;
+            }
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        throw new AssertionError();
+    }
+    public boolean dfs(ArrayList<Integer>[] graph, int source, int target) {
+        if (!seen.contains(source)) {
+            seen.add(source);
+            if (source == target) return true;
+            for (int nei: graph[source]) {
+                if (dfs(graph, nei, target)) return true;
+            }
+        }
+        return false;
+    }
+}
+
+// https://leetcode.com/problems/redundant-connection-ii
+
+/*
+Return an edge that can be removed so that the resulting graph 
+is a rooted tree of N nodes. 
+If there are multiple answers, return the answer that occurs 
+last in the given 2D-array.
+
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+Explanation: The given directed graph will be like this:
+  1
+ / \
+v   v
+2-->3
+
+Input: [[1,2], [2,3], [3,4], [4,1], [1,5]]
+Output: [4,1]
+Explanation: The given directed graph will be like this:
+5 <- 1 -> 2
+     ^    |
+     |    v
+     4 <- 3
+
+*/
+
+
+// Approach DFS Time & Space O(n)
+
+
+class Solution {
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int N = edges.length;
+        Map<Integer, Integer> parent = new HashMap();
+        List<int[]> candidates = new ArrayList();
+        for (int[] edge: edges) {
+            if (parent.containsKey(edge[1])) {
+                candidates.add(new int[]{parent.get(edge[1]), edge[1]});
+                candidates.add(edge);
+            } else {
+                parent.put(edge[1], edge[0]);
+            }
+        }
+
+        int root = orbit(1, parent).node;
+        if (candidates.isEmpty()) {
+            Set<Integer> cycle = orbit(root, parent).seen;
+            int[] ans = new int[]{0, 0};
+            for (int[] edge: edges) {
+                if (cycle.contains(edge[0]) && cycle.contains(edge[1])) {
+                    ans = edge;
+                }
+            }
+            return ans;
+        }
+
+        Map<Integer, List<Integer>> children = new HashMap();
+        for (int v: parent.keySet()) {
+            int pv = parent.get(v);
+            if (!children.containsKey(pv))
+                children.put(pv, new ArrayList<Integer>());
+            children.get(pv).add(v);
+        }
+
+        boolean[] seen = new boolean[N+1];
+        seen[0] = true;
+        Stack<Integer> stack = new Stack();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            if (!seen[node]) {
+                seen[node] = true;
+                if (children.containsKey(node)) {
+                    for (int c: children.get(node))
+                        stack.push(c);
+                }
+            }
+        }
+        for (boolean b: seen) if (!b)
+            return candidates.get(0);
+        return candidates.get(1);
+    }
+
+    public OrbitResult orbit(int node, Map<Integer, Integer> parent) {
+        Set<Integer> seen = new HashSet();
+        while (parent.containsKey(node) && !seen.contains(node)) {
+            seen.add(node);
+            node = parent.get(node);
+        }
+        return new OrbitResult(node, seen);
+    }
+
+}
+class OrbitResult {
+    int node;
+    Set<Integer> seen;
+    OrbitResult(int n, Set<Integer> s) {
+        node = n;
+        seen = s;
+    }
+}
