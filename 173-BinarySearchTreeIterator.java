32e54ee3281da0f2016419ddde922da5efb2ea49
hwhollywu <hwhollywu@gmail.com>
2019-01-24 09:11:53 -0800

    173-BinarySearchTreeIterator.java

--- 173-BinarySearchTreeIterator.java
+++ 173-BinarySearchTreeIterator.java
@@ -22,3 +22,49 @@ iterator.hasNext(); // return false
 */
 
 // use stack because want to get the iterator that it most recently inserted 
+
+public class BSTIterator {
+    
+    private Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null){
+            stack.push(cur);
+            if(cur.left != null)
+                cur = cur.left;
+            else
+                break;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode cur = node;
+        // traversal right branch
+        if(cur.right != null){
+            cur = cur.right;
+            while(cur != null){
+                stack.push(cur);
+                if(cur.left != null)
+                    cur = cur.left;
+                else
+                    break;
+            }
+        }
+        return node.val;
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
